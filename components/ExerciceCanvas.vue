11ca23eca961e3cf60b063c3b55378d42a5a3b5d
Bertyn Boulikou <bboulikou@gmail.com>
2023-09-13 10:28:00 +0200

    feat add stackblitz

---
+++ components/ExerciceCanvas.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import sdk, { EmbedOptions } from "@stackblitz/sdk";
+
+/* sdk.embedProjectId("embed", "css-custom-prop-color-values", {
+  openFile: "index.ts",
+}); */
+
+const embed = ref(null);
+
+watchEffect(() => {
+  if (embed.value) {
+    embed.value.appendChild(document.createElement("div"));
+    sdk.embedProjectId(embed.value.children[0], "css-custom-prop-color-values");
+  }
+});
+</script>
+
+<template>
+  <client-only>
+    <div ref="embed" id="embed"></div>
+  </client-only>
+</template>
+
+<style lang="postcss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <h1>Course</h1>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque quasi
+      aliquam doloribus tempore facere tenetur ullam vero ipsam provident
+      temporibus iste incidunt consectetur harum omnis atque voluptate sed,
+      optio suscipit!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque quasi
+      aliquam doloribus tempore facere tenetur ullam vero ipsam provident
+      temporibus iste incidunt consectetur harum omnis atque voluptate sed,
+      optio suscipit!
+    </p>
+
+    <ExerciceCanvas />
+  </div>
+</template>
+
+<style lang="postcss" scoped></style>
